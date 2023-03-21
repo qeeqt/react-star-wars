da32071bad868beeab6e5e1b886abd5bcfd52689
qeeqt <vvvooovvvaaa5%gmail.com>
2023-03-21 13:57:25 +0200

    useState

--- src/conteiners/App/App.jsx
+++ src/conteiners/App/App.jsx
@@ -2,9 +2,24 @@ import styles from './App.module.css';
 import React from 'react';
 import { getIpiResourse } from '../../utils/network'
 
+import { useState } from 'react';
+
 const App = () => {
+  const [count, setCount] = useState(0);
+  function handl1() {
+    setCount(count + 1)
+  }
+  function handl2() {
+    setCount(count - 1)
+  }
+
   return (
-    <h1 className={styles.header}>Hello vova</h1>
+
+    <div>
+      <h1>count={count}</h1>
+      <button count={count} onClick={handl1} />
+      <button count={count} onClick={handl2} />
+    </div>
   )
 }
 
